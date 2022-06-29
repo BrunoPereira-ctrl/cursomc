99fbc7e0db09b1dbf00c88f2fb013d31f19e3082
Bruno Pereira <brunopereiradosreis369@gmail.com>
2022-06-29 19:38:23 -0300

    ajuste no endpoint /categorias/{id}

--- src/main/java/com/brunopereira/cursomc/services/CategoriaService.java
+++ src/main/java/com/brunopereira/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.brunopereira.cursomc.domain.Categoria;
 import com.brunopereira.cursomc.repositories.CategoriaRepository;
+import com.brunopereira.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -18,6 +19,6 @@ public class CategoriaService {
 		
 		Optional<Categoria> obj = repo.findById(id);
 		
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado: "+ id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
